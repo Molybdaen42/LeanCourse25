b99da25de3d9d84470d0a9b73ac537000edae196
Nora Depenheuer <97042192+N-DPH@users.noreply.github.com>
2025-10-28 12:47:15 +0100

    Downloading the sheet

---
+++ LeanCourse25/Assignments/InPersonSheet.lean
@@ -0,0 +1,127 @@
+import Mathlib.Data.Real.Basic
+import Mathlib.Tactic
+import Mathlib.Util.Delaborators
+
+section Functions
+
+def FnUb (f : ℝ → ℝ) (a : ℝ) : Prop :=
+  ∀ x, f x ≤ a
+
+def FnLb (f : ℝ → ℝ) (a : ℝ) : Prop :=
+  ∀ x, a ≤ f x
+
+variable (f g : ℝ → ℝ) (a b : ℝ)
+
+example (hfa : FnLb f a) (hgb : FnLb g b) : FnLb (fun x ↦ f x + g x) (a + b) :=
+  sorry
+
+example (nnf : FnLb f 0) (nng : FnLb g 0) : FnLb (fun x ↦ f x * g x) 0 :=
+  sorry
+
+example (hfa : FnUb f a) (hgb : FnUb g b) (nng : FnLb g 0) (nna : 0 ≤ a) :
+    FnUb (fun x ↦ f x * g x) (a * b) :=
+  sorry
+
+def FnEven (f : ℝ → ℝ) : Prop :=
+  ∀ x, f x = f (-x)
+
+def FnOdd (f : ℝ → ℝ) : Prop :=
+  ∀ x, f x = -f (-x)
+
+example (hfa : FnLb f a) (hgb : FnLb g b) : FnLb (fun x ↦ f x + g x) (a + b) :=
+  sorry
+
+example (nnf : FnLb f 0) (nng : FnLb g 0) : FnLb (fun x ↦ f x * g x) 0 :=
+  sorry
+
+example (hfa : FnUb f a) (hgb : FnUb g b) (nng : FnLb g 0) (nna : 0 ≤ a) :
+    FnUb (fun x ↦ f x * g x) (a * b) :=
+  sorry
+
+example {c : ℝ} (mf : Monotone f) (nnc : 0 ≤ c) : Monotone fun x ↦ c * f x :=
+  sorry
+
+example (mf : Monotone f) (mg : Monotone g) : Monotone fun x ↦ f (g x) :=
+  sorry
+
+end Functions
+
+section existential
+
+def FnHasUb (f : ℝ → ℝ) :=
+  ∃ a, FnUb f a
+
+def FnHasLb (f : ℝ → ℝ) :=
+  ∃ a, FnLb f a
+
+variable {f g : ℝ → ℝ}
+
+example (lbf : FnHasLb f) (lbg : FnHasLb g) : FnHasLb fun x ↦ f x + g x := by
+  sorry
+
+example {c : ℝ} (ubf : FnHasUb f) (h : c ≥ 0) : FnHasUb fun x ↦ c * f x := by
+  sorry
+
+variable {α : Type*} [CommRing α]
+
+def SumOfSquares (x : α) :=
+  ∃ a b, x = a ^ 2 + b ^ 2
+
+theorem sumOfSquares_mul {x y : α} (sosx : SumOfSquares x) (sosy : SumOfSquares y) :
+    SumOfSquares (x * y) := by
+  sorry
+
+example {a b c : ℕ} (divab : a ∣ b) (divac : a ∣ c) : a ∣ b + c := by
+  sorry
+
+open Function
+
+example {c : ℝ} (h : c ≠ 0) : Surjective fun x ↦ c * x := by
+  sorry
+
+end existential
+
+section negation
+
+variable (f : ℝ → ℝ)
+
+example (h : ∀ a, ∃ x, f x < a) : ¬FnHasLb f :=
+  sorry
+
+example : ¬FnHasUb fun x ↦ x :=
+  sorry
+
+example : ¬∀ {f : ℝ → ℝ}, Monotone f → ∀ {a b}, f a ≤ f b → a ≤ b := by
+  sorry
+
+end negation
+
+section conjunction
+
+theorem aux {x y : ℝ} (h : x ^ 2 + y ^ 2 = 0) : x = 0 :=
+  sorry
+
+example (x y : ℝ) : x ^ 2 + y ^ 2 = 0 ↔ x = 0 ∧ y = 0 :=
+  sorry
+
+theorem not_monotone_iff {f : ℝ → ℝ} : ¬Monotone f ↔ ∃ x y, x ≤ y ∧ f x > f y := by
+  sorry
+
+example : ¬Monotone fun x : ℝ ↦ -x := by
+  sorry
+
+end conjunction
+
+section disjunction
+
+-- use `eq_zero_or_eq_zero_of_mul_eq_zero` to prove the following
+
+#check eq_zero_or_eq_zero_of_mul_eq_zero
+
+example {x : ℝ} (h : x ^ 2 = 1) : x = 1 ∨ x = -1 := by
+  sorry
+
+example {x y : ℝ} (h : x ^ 2 = y ^ 2) : x = y ∨ x = -y := by
+  sorry
+
+end disjunction
